e164bdcafcc1a22fb2f6d768396764f1e88cf362
khacduyet <duyetbuikhac@gmail.com>
2022-11-19 21:12:01 +0700

    duyet

--- src/pages/quanlydongphi/OverPanel.jsx
+++ src/pages/quanlydongphi/OverPanel.jsx
@@ -8,6 +8,11 @@ export default function OverPanel({ listData, isXeNgoai }) {
         className="p-datatable p-component p-datatable-selectable p-datatable-responsive-stack"
         style={{ width: "500px" }}
         value={listData}
+        paginatorLeft={"Tổng số bản ghi " + listData?.length}
+        paginatorClassName="justify-content-end"
+        paginator
+        first={0}
+        rows={5}
       >
         <Column
           bodyClassName="text-center"
@@ -24,25 +29,32 @@ export default function OverPanel({ listData, isXeNgoai }) {
           header="Mã"
         ></Column>
         <Column
+          filter
+          filterPlaceholder="Search by name"
           style={{ width: "8%" }}
           field="Ten"
           headerClassName="text-center"
           bodyClassName="text-center"
           header="Tên"
         ></Column>
-        <Column
-          style={{ width: "10%" }}
-          field="ChuSoHuu"
-          headerClassName="text-center"
-          bodyClassName="text-center"
-          header={() => {
-            return isXeNgoai ? "Biển kiểm soát" : "Chủ sở hữu";
-          }}
-          body={(rowData) => {
-            console.log(rowData);
-            return isXeNgoai ? rowData.ChuSoHuu : rowData.BienKiemSoat;
-          }}
-        ></Column>
+        {!isXeNgoai && (
+          <Column
+            style={{ width: "10%" }}
+            field="ChuSoHuu"
+            headerClassName="text-center"
+            bodyClassName="text-center"
+            header="Chủ sở hữu"
+          ></Column>
+        )}
+        {isXeNgoai && (
+          <Column
+            style={{ width: "10%" }}
+            field="BienKiemSoat"
+            headerClassName="text-center"
+            bodyClassName="text-center"
+            header="Biển kiểm soát"
+          ></Column>
+        )}
       </DataTable>
     </>
   );
